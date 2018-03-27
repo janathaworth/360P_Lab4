0742c7c5e63ed41484dca363ff98fc22617bea6a
janat <janat.haworth@utexas.edu>
2018-03-27 10:08:51 -0500

    combiner function

--- TextAnalyzer.java
+++ TextAnalyzer.java
@@ -78,17 +78,15 @@ public class TextAnalyzer extends Configured implements Tool {
             throws IOException, InterruptedException
         {
             // Implementation of you combiner function
-        	HashMap<String, SimpleEntry<String, Integer>> map = new HashMap<String, SimpleEntry<String, Integer>>();
+        	HashMap<String, Integer> map = new HashMap<String, Integer>();
         	String query_word;
         	for (SimpleEntry<String, Integer> entry: tuples) {
         		query_word = entry.getKey();
         		if (map.containsKey(query_word)) {
-    				SimpleEntry<String, Integer> temp = map.get(query_word);
-    				temp.setValue(temp.getValue() + 1);
-    				map.put(query_word, temp);
+    				map.put(query_word, map.get(query_word) + 1);
     			}
     			else {
-    				map.put(query_word, new SimpleEntry(query_word, 1));
+    				map.put(query_word, new Integer(1));
     			}
         	}
         	for (String s: map.keySet()) {
